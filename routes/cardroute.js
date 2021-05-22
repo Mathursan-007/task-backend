3cfe467b2ff4fb691c76bbabea16cf1119bae2dc
Mathursan-007 <63546506+Mathursan-007@users.noreply.github.com>
2021-05-22 20:07:32 +0530

    router updated

--- routes/cardroute.js
+++ routes/cardroute.js
@@ -4,22 +4,22 @@ const {addCard,getCards,addList,handleList} = require('../api/cards.api');
 
 router.get('/',(req,res)=>{
 
-    req.body=getCards();
-    console.log(req.body);
+    res.send(getCards());
+
 });
 
 router.post('/',(req,res)=>{
     let card = req.body
     card =addCard(card)
-    res.body=card
-    console.log(res.body);
+    res.send(card)
 });
 
 router.post('/lists',(req,res)=>{
+
     let list=req.body
     list =addList(list);
-    res.body=list;
-    console.log(res.body)
+    res.send(list);
+
 })
 
 router.patch('/lists/:id',(req,res)=>{
@@ -28,8 +28,7 @@ router.patch('/lists/:id',(req,res)=>{
     let state=req.body;
     let id=req.params.id;
     let list=handleList(id,state);
-    res.body=list;
-    console.log(res.body);
+    res.send(list);
 
 })
 
