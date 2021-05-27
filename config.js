b8d18b01376f5d02d024846ce3c8dc2784cd5661
Mathursan-007 <63546506+Mathursan-007@users.noreply.github.com>
2021-05-27 16:33:08 +0530

    config

---
+++ config.js
@@ -0,0 +1,10 @@
+const CLOUD_NAME="dl4psqagn"
+const API_KEY="587545497235784"
+const API_SECRET="94aSZhVknWE_fcJFcqDzZ43FnR8"
+
+const URI="mongodb+srv://admin:admin@first.yueye.mongodb.net/conferenceDB?retryWrites=true&w=majority"
+
+
+module.exports={
+    CLOUD_NAME,API_KEY,API_SECRET,URI
+}
