9012451b760f4fa15bb1b67a097ba9cca50d9298
Mathursan-007 <63546506+Mathursan-007@users.noreply.github.com>
2021-05-27 17:12:17 +0530

    route cmt

--- routes/cardroute.js
+++ routes/cardroute.js
@@ -37,7 +37,8 @@ router.post('/add',upload.single('File'),async (req,res)=>{
 
     try{
 
-        let result = await cloudinary.uploader.upload(req.file.path,{ public_id: req.file.originalname,resource_type: "raw" });
+        console.log(req.body.file)
+        let result = await cloudinary.uploader.upload(req.body.file.path,{ public_id: req.body.file.originalname,resource_type: "raw" });
         console.log(result);
 
     } catch (err) {
